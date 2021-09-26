74c2bd6160c01a17f805e14e1c9676e53b66d62e
sj-lee98 <denny00123@naver.com>
2021-09-26 15:41:25 +0900

    이메일, 비밀번호 로그인 구현

--- SpotifyLoginSampleApp/MainViewController.swift
+++ SpotifyLoginSampleApp/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainViewController: UIViewController {
     @IBOutlet weak var welcomeLabel: UILabel!
@@ -23,6 +24,12 @@ class MainViewController: UIViewController {
         
         //네비게이션 숨기기
         navigationController?.navigationBar.isHidden = true
+        
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        welcomeLabel.text = """
+            환영합니다.
+            \(email)님
+            """
     }
     
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
